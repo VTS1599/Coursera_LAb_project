ec7c971f5479ca4657ae77d4dc32216b3d46bd1a
VTS1599 <vts1599@gmail.com>
2024-09-25 21:59:40 +0530

    initial commit

---
+++ src/Components/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice.js';
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+export default store;
